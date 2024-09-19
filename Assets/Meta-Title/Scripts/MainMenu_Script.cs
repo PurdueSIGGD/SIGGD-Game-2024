fc97cfc864c8173d5576d346a0633f97e769b7be
Benjamin Lobos <blobosle@purdue.edu>
2024-09-19 00:04:45 -0400

    Added code readability
    Formatting and comments.

--- Assets/Meta-Title/Scripts/MainMenu_Script.cs
+++ Assets/Meta-Title/Scripts/MainMenu_Script.cs
@@ -3,8 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/*
+ * If in doubt of what any of this does or works consult with the META SYSTEMS team ;-; 
+ */
+
 public class MainMenu_Script : MonoBehaviour
 {
+    /* All public button functions are called by the UI buttons */
     public void PlayButton(string sceneName)
     {
         StartCoroutine(LoadYourAsyncScene(sceneName));
@@ -19,19 +24,21 @@ public class MainMenu_Script : MonoBehaviour
     {
         Application.Quit();
         
-        // Keep this line below to simulate the quit on the Unity editor plsss
+        /* Keep this line below to simulate the quit on the Unity editor plsss */
+        
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
 #endif
     }
     
+    /* Used to async load the main play scene */
     IEnumerator LoadYourAsyncScene(string sceneName)
     {
         AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
 
         while (!asyncLoad.isDone)
         {
-            // You can add a loading progress bar here if you want
+            // We could add a loading progress bar here if we really wanted to
             yield return null;
         }
     }
