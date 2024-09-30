a722f30b5dd3c5138aabd191dba1263cc230bbc2
V-S-Coder <144750932+V-S-Coder@users.noreply.github.com>
2024-09-30 18:27:40 -0400

    Update Transparent Platform Script
    i updated it so the players collider ignores the platforms which won't turn off the platforms collider.

--- Assets/Scripts/TransparentPlatdormScript.cs
+++ Assets/Scripts/TransparentPlatdormScript.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/// <summary>
+/// This code allows players to jump through a platform and land on it while also allowing them
+/// to go through it to get down.
+/// </summary>
 public class TransparentPlatdormScript : MonoBehaviour
 {
     private Collider2D _collider;
     private bool _playerOnPlatform;
-    public GameObject player;
+    public GameObject player; // gets the game object of the player
 
     private void Start()
     {
@@ -18,17 +21,26 @@ public class TransparentPlatdormScript : MonoBehaviour
     {
         if (_playerOnPlatform && Input.GetAxisRaw("Vertical") < 0)
         {
-            _collider.enabled = false;
+            //_collider.enabled = false;
             StartCoroutine(EnableCollider());
         }
     }
-
+    /// <summary>
+    /// has the players collider igonore the platform collider and then pauses the
+    /// code for 1 second in which afterwards the colliders are back to normal.
+    /// </summary>
     private IEnumerator EnableCollider()
     {
+        Physics2D.IgnoreCollision(player, _collider);
         yield return new WaitForSeconds(1f);
-        _collider.enabled = true;
+        Physics2D.IgnoreCollision(player, _collider, false);
+      //  _collider.enabled = true;
     }
-
+    /// <summary>
+    /// Detects if the player is on the platform
+    /// </summary>
+    /// <param name="other"></param>
+    /// <param name="value"></param>
     private void SetPlayerOnPlatform(Collision2D other, bool value)
     {
        
