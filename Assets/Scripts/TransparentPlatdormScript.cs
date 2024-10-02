e2c46feeb8440093c6ad2ce4bcb9f5f1ae66fef7
V-S-Coder <144750932+V-S-Coder@users.noreply.github.com>
2024-10-02 19:46:19 -0400

    TransparentPlatformScript Changed
    I changed the script so that the players collider ignores the platform collider instead of turning off the platform collider.

--- Assets/Scripts/TransparentPlatdormScript.cs
+++ Assets/Scripts/TransparentPlatdormScript.cs
@@ -1,34 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Dependencies.Sqlite;
 using UnityEngine;
 
 public class TransparentPlatdormScript : MonoBehaviour
 {
+
     private Collider2D _collider;
     private bool _playerOnPlatform;
     public GameObject player;
+    private Collider2D playercollider;
 
     private void Start()
     {
         _collider = GetComponent<Collider2D>();
-
+        playercollider = player.GetComponent<Collider2D>();
     }
 
     private void Update()
     {
         if (_playerOnPlatform && Input.GetAxisRaw("Vertical") < 0)
         {
-            _collider.enabled = false;
+           // _collider.enabled = false;
             StartCoroutine(EnableCollider());
         }
     }
-
+    /// <summary>
+    /// makes the players collider ignore the platforms collider then stops the code for 1 second in when then it reverts the colliders to normal
+    /// </summary>
     private IEnumerator EnableCollider()
     {
+        Physics2D.IgnoreCollision(playercollider, _collider);
         yield return new WaitForSeconds(1f);
-        _collider.enabled = true;
+       // _collider.enabled = true;
+       Physics2D.IgnoreCollision(playercollider, _collider, false);
     }
-
+    /// <summary>
+    /// This detects if the player is on the platform
+    /// </summary>
+    /// <param name="other"></param>
+    /// <param name="value"></param>
     private void SetPlayerOnPlatform(Collision2D other, bool value)
     {
        
@@ -37,12 +48,18 @@ public class TransparentPlatdormScript : MonoBehaviour
             _playerOnPlatform = value;
         }
     }
-
+    /// <summary>
+    /// Checks to see if an object collides with the platform
+    /// </summary>
+    /// <param name="other"></param>
     private void OnCollisionEnter2D(Collision2D other)
     {
         SetPlayerOnPlatform(other, true);
     }
-
+ /// <summary>
+ /// Checks to see if an object extis the platform
+ /// </summary>
+ /// <param name="other"></param>
     private void OnCollisionExit2D(Collision2D other)
     {
         SetPlayerOnPlatform(other, true);
