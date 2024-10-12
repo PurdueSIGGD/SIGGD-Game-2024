985f06f2998bfdd1a87bb8879dacbbd2aaf6e6c0
PurdueAGao <njAngelaGao@gmail.com>
2024-10-11 20:14:32 -0400

    Delete unnecessary comments

--- Assets/Scenes/SkillTreeOne.cs
+++ Assets/Scenes/SkillTreeOne.cs
@@ -56,7 +56,6 @@ public class SkillTreeOne : BasicSkillTree
     // Update is called once per frame
     void Update()
     {
-        //Debug.Log(this.branchList[0].GetSkillList().Count);
         
     }
 }
