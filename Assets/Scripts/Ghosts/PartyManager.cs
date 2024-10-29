da5420a9f472bce7375c5944bf42f93e517fdb9d
DinoSage <dinosage5632@gmail.com>
2024-10-28 20:40:44 -0400

    [%] Cleaned up formatting for Ghosts

--- Assets/Scripts/Ghosts/PartyManager.cs
+++ Assets/Scripts/Ghosts/PartyManager.cs
@@ -9,71 +9,14 @@ using TMPro;
 
 public class PartyManager : MonoBehaviour
 {
-    // Party Customization
-    //[SerializeField]
-    //private Color inPartyColor; // Indicate ghosts are in the party
-    //[SerializeField]
-    //private Color notInPartyColor; // Indicate ghosts are not in the party
     [SerializeField] 
     private int ghostMajorLimit; // Amount of major ghosts player can wield at one time
-    //public InputAction partySettings; // Button to start removing ghosts
-    //public InputAction chooseMajorGhostRemove; // Select ghost to remove
-    //private bool toggleParty; // Toggle to prevent removing same index multiple times
     
     [SerializeField]
     private float range; // Temporary variable to store find ghost range
 
     // Major Ghosts
     private List<GhostIdentity> ghostMajorList = new List<GhostIdentity>(); // List of each major ghost
-
-    /*void OnEnable() 
-    {
-        partySettings.Enable();
-        chooseMajorGhostRemove.Enable();
-    }
-
-    void OnDisable() 
-    {
-        partySettings.Disable();
-        chooseMajorGhostRemove.Disable();
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        toggleParty = false;
-    }*/
-
-    /*// Update is called once per frame
-    void Update()
-    {
-        if (partySettings.triggered) {
-            toggleParty = !toggleParty;
-            if (toggleParty) {
-                Debug.Log("Ready to remove major ghost. Press [B] to cancel.");
-            } else {
-                Debug.Log("Exiting party customization...");
-            }
-        }
-        if (toggleParty) {
-            try { // there is probably a better way to do this
-                if (chooseMajorGhostRemove.ReadValue<float>() == -1) {
-                    RemoveMajorGhost(0);
-                } else if (chooseMajorGhostRemove.ReadValue<float>() == 1) {
-                    RemoveMajorGhost(1);
-                }
-            } catch (System.Exception e) {}
-        }
-    }
-
-    public void OnCollisionEnter2D(Collision2D collision) 
-    {
-        GhostIdentity ghost = collision.gameObject.GetComponent<GhostIdentity>();
-        if (collision.gameObject.CompareTag("GhostMajor") && ghostMajorList.Count < ghostMajorLimit) {
-            AddMajorGhost(ghost);
-        }
-    }*/
-
 
     /// <summary>
     /// Temporary function to find and add ghosts nearby to party if space available. Max 1 ghost per function call
