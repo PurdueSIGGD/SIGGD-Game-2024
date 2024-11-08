67f8f2677ef12a5db75d83600bed4661a95a8e98
WangXin1507 <simonwang421@outlook.com>
2024-11-08 16:51:13 -0500

    preparing for loading animation for gun with legs

--- Assets/GunWithLegs.cs
+++ Assets/GunWithLegs.cs
@@ -8,15 +8,24 @@ public class GunWithLegs : EnemyStateManager
     [SerializeField] protected Transform gunShoot;
     [SerializeField] protected Transform rangeOrig;
 
+    [SerializeField] protected Transform projectile;
+
     protected override ActionPool GenerateActionPool()
     {
-        Action kick = new(gunKick, 4.0f, 3f, "EMPTY");
-        Action shoot = new(gunShoot, 4.0f, 7f, "EMPTY");
+        Action kick = new(gunKick, 4.0f, 3f, "Gun_W_Leg_Kick");
+        Action shoot = new(gunShoot, 4.0f, 7f, "Gun_W_Leg_Shoot");
 
-        Action move = new(null, 0.0f, 0.0f, "kick");
-        Action idle = new(null, 0.0f, 0.0f, "shoot");
+        Action move = new(null, 0.0f, 0.0f, "move");
+        Action idle = new(null, 0.0f, 0.0f, "idle");
 
         return new ActionPool(new List<Action> { kick, shoot }, move, idle);
+    }
+
+    protected void OnShootEvent()
+    {
+        Instantiate(projectile, rangeOrig.position, transform.rotation);
+
+        rb.AddForce(transform.right * -3, ForceMode2D.Impulse);
     }
 
 
