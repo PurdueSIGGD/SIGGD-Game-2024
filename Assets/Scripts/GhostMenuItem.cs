834d8e17a597bc84805df8d01e4310d5780c889e
SpaceMonkeyy86 <jackreynolds9128@gmail.com>
2024-11-08 21:22:49 -0500

    Add ghost item prefab

---
+++ Assets/Scripts/GhostMenuItem.cs
@@ -0,0 +1,26 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GhostMenuItem : MonoBehaviour
+{
+    [SerializeField] public TextMeshProUGUI textComponent;
+    [SerializeField] public Button buttonComponent;
+
+    public GhostIdentity identity;
+    public PartyEditor menu;
+
+    void Start()
+    {
+        textComponent.SetText(identity.name);
+        buttonComponent.onClick.AddListener(() =>
+        {
+            menu.Select(this);
+        });
+    }
+
+    public void SetSelected(bool selected)
+    {
+
+    }
+}
