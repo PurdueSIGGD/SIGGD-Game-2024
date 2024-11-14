210bbb7b9c54a47f36ee8e6f81736e5bb9ec3b60
JasonX354 <xjason354@gmail.com>
2024-11-13 20:26:50 -0500

    Testing objects made. Currently empty.

---
+++ Assets/Scripts/Data-Management/DataManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Purpose: To relay data between different scenes. Test scenes are located in
+/// Scenes/Data-Management. 
+/// 
+/// Objective: Create a static instance in which two scenes will change its value.
+/// </summary>
+public class DataManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
