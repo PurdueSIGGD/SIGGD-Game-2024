fa41fe9a5678fdf9a0f79310c73718ce0d8ac087
Lance <lance.salvad@gmail.com>
2024-11-13 20:35:07 -0500

    UI Very Basic Implementation

--- Assets/Scripts/Meta Systems/WorldInteractable.cs
+++ Assets/Scripts/Meta Systems/WorldInteractable.cs
@@ -1,11 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public abstract class WorldInteractable : MonoBehaviour
 {
     [SerializeField] private GameObject player;
     [SerializeField] private float activationRange;
+    [SerializeField] private Camera cam;
+    [SerializeField] private UIBehaviour menu;
 
     protected List<WorldInteractableOption> options;
 
@@ -22,13 +26,21 @@ public abstract class WorldInteractable : MonoBehaviour
     // Update function checks if player is in range, then updates and displays the options list.
     private void Update()
     {
+        menu.enabled = false;
+
         Vector3 dist = player.transform.position - transform.position;
         if (dist.magnitude > activationRange) return;
-        
+
+        menu.enabled = true;
+        menu.transform.position = cam.WorldToScreenPoint(transform.position);
         UpdateOptions();
+
+
+        /*
         foreach (WorldInteractableOption opt in options)
         {
-            Debug.Log(opt.GetName());
+        
         }
+        */
     }
 }
