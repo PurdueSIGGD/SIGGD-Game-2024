97132e01dd5be3351964bb7cba89d493e03de408
DinoSage <dinosage5632@gmail.com>
2024-12-02 22:36:20 -0500

    [%] Fixed Glide

--- Assets/Scripts/Player/Move.cs
+++ Assets/Scripts/Player/Move.cs
@@ -3,30 +3,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Experimental.GraphView;
 using UnityEditor.SceneTemplate;
+using UnityEditor.U2D;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Move : MonoBehaviour
 {
-    private InputAction playerActionMovement;
+    private InputAction moveInput;
     private InputAction playerActionDown;
     private Rigidbody2D rb;
     public Boolean doubleJump = true;
 
     private Stats stats;
-    private int maxSpeedIdx;
-    private int accelerationIdx;
+    private int maxRunningSpeedIdx;
+    private int runningAccelIdx;
+    private int runningDeaccelIdx;
+
+
+    private int maxGlideSpeedIdx;
+    private int glideAccelIdx;
+    private int glideDeaccelIdx;
+
+    private bool gliding = false;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        playerActionMovement = GetComponent<PlayerInput>().actions.FindAction("Movement");
-        playerActionDown = GetComponent<PlayerInput>().actions.FindAction("Down");
+        moveInput = GetComponent<PlayerInput>().actions.FindAction("Movement");
         rb = GetComponent<Rigidbody2D>();
         stats = GetComponent<Stats>();
-        maxSpeedIdx = stats.GetStatIndex("Max Speed");
-        accelerationIdx = stats.GetStatIndex("Acceleration");
 
+        maxRunningSpeedIdx = stats.GetStatIndex("Max Running Speed");
+        runningAccelIdx = stats.GetStatIndex("Running Acceleration");
+        runningDeaccelIdx = stats.GetStatIndex("Running Deacceleration");
+
+        maxGlideSpeedIdx = stats.GetStatIndex("Max Glide Speed");
+        glideAccelIdx = stats.GetStatIndex("Glide Acceleration");
+        glideDeaccelIdx = stats.GetStatIndex("Glide Deacceleration");
     }
 
     // Update is called once per frame
@@ -40,22 +54,35 @@ public class Move : MonoBehaviour
     /// </summary>
     private void Movement()
     {
-        float input = playerActionMovement.ReadValue<float>();
+        float accel, maxSpeed, deaccel = 0;
+        if (gliding)
+        {
+            accel = stats.ComputeValue(glideAccelIdx);
+            maxSpeed = stats.ComputeValue(maxGlideSpeedIdx);
+            deaccel = stats.ComputeValue(glideDeaccelIdx);
+        } else
+        {
+            accel = stats.ComputeValue(runningAccelIdx);
+            maxSpeed = stats.ComputeValue(maxRunningSpeedIdx);
+            deaccel = stats.ComputeValue(runningDeaccelIdx);
+        }
+
+        float input = moveInput.ReadValue<float>();
         Vector2 newVel = new Vector2(0, 0);
 
         // accelerates player in direction of input
-        newVel.x = rb.velocity.x + input * stats.ComputeValue(accelerationIdx);
+        newVel.x = rb.velocity.x + input * accel;
 
         // caps top speed
-        if (newVel.magnitude > stats.ComputeValue(maxSpeedIdx))
+        if (newVel.magnitude > maxSpeed)
         {
-            newVel = newVel.normalized * stats.ComputeValue(maxSpeedIdx);
+            newVel = newVel.normalized * maxSpeed;
         }
 
         // deaccelerate if no input
         if (input == 0)
         {
-            newVel.x *= 0.9f;
+            newVel.x *= deaccel;
         }
 
         // keep updating y velocity
@@ -65,35 +92,13 @@ public class Move : MonoBehaviour
         rb.velocity = newVel;
     }
 
-    /// <summary>
-    /// Method is called once the player presses the button binded to "Jump" in Player Actions also controlls double jump
-    /// </summary>
-    /*private void OnJump()
+    public void StartGlide()
     {
-        if (OnGround())
-        {
-            doubleJump = true;
-            rb.gravityScale = 4;
-            rb.velocity = new Vector2(rb.velocity.x, 0);
-            rb.AddForce(new Vector2(0, 1) * stats.ComputeValue(jumpForceIdx), ForceMode2D.Impulse);
-        }
-        //if double jump has not been used yet and in the air, turn off double jump
-        else if (doubleJump)
-        {
-            doubleJump = false;
-            rb.gravityScale = 4;
-            rb.velocity = new Vector2(rb.velocity.x, 0);
-            rb.AddForce(new Vector2(0, 1) * stats.ComputeValue(jumpForceIdx), ForceMode2D.Impulse);
-        }
-    }*/
+        gliding = true;
+    }
 
-    /// <summary>
-    /// Method that uses a circle cast to determine whether or not the player in on the ground
-    /// </summary>
-    /// <returns>Boolean of whether or not the player is on the ground</returns>
-    public Boolean OnGround()
+    public void StopGlide()
     {
-        LayerMask mask = LayerMask.GetMask("ground");
-        return Physics2D.CircleCast(transform.position + Vector3.down, 0.5f, Vector2.down.normalized, mask);
+        gliding = false;
     }
 }
--- Assets/Scripts/Player/glide.cs
+++ Assets/Scripts/Player/glide.cs
@@ -3,68 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class Glide : MonoBehaviour, IGlideMove
+public class Glide : MonoBehaviour
 {
     //initiate variables
-    //the constant fall speed for glide
-    [SerializeField] private float glideFallSpeed = 2f;
-    private Move moveCharacter;
     private Rigidbody2D rb;
-    private InputAction playerActionJump;
-    private bool isGliding = false;
+    private Stats stats;
+
+    private int glideFallSpeedIdx;
 
     private void Start()
     {
-        moveCharacter = GetComponent<Move>();
         rb = GetComponent<Rigidbody2D>();
-        playerActionJump = GetComponent<PlayerInput>().actions.FindAction("Jump");
+        stats = GetComponent<Stats>();
+
+        glideFallSpeedIdx = stats.GetStatIndex("Glide Fall Speed");
     }
 
-    private void Update()
+    public void StartGlide()
     {
-        //if player is not on ground and if all jumps are used up
-        if (!moveCharacter.OnGround() && !moveCharacter.doubleJump)
-        {
-            HandleGlide();
-        }
-        else
-        {
-            //if on ground then no glide
-            StopGlide();
-        }
-    }
-
-    private void HandleGlide()
-    {
-        //if jump is pressed and no velocity
-        if (playerActionJump.IsPressed() && rb.velocity.y <= 0)
-        {
-            if (!isGliding)
-            {
-                StartGlide();
-            }
-            rb.velocity = new Vector2(rb.velocity.x, -glideFallSpeed);
-        }
-        else if (isGliding)
-        {
-            StopGlide();
-        }
-    }
-
-    private void StartGlide()
-    {
-        isGliding = true;
-        //disabling gravity
+        rb.velocity = new Vector2(rb.velocity.x, -1 * stats.ComputeValue(glideFallSpeedIdx));
         rb.gravityScale = 0;
     }
 
-    private void StopGlide()
+    public void StopGlide()
     {
-        isGliding = false;
         rb.gravityScale = 4;
-    }
-    public bool GetBool()
-    {
-        return isGliding;
     }
 }
