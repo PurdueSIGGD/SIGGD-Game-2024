c71f2282ba7394b98616fe0395430b0669c63223
Lance <lance.salvad@gmail.com>
2025-01-22 20:24:10 -0500

    Implemented placeholder menu spawning

--- Assets/Scripts/Meta Systems/WorldInteractable.cs
+++ Assets/Scripts/Meta Systems/WorldInteractable.cs
@@ -9,8 +9,8 @@ public abstract class WorldInteractable : MonoBehaviour
     [SerializeField] private GameObject player;
     [SerializeField] private float activationRange;
     [SerializeField] private Camera cam;
-    [SerializeField] private UIBehaviour menu;
-    private CanvasGroup menuGroup;
+    [SerializeField] private Canvas canvas;
+    [SerializeField] private RectTransform rectTransform;
 
     protected List<WorldInteractableOption> options;
 
@@ -21,20 +21,22 @@ public abstract class WorldInteractable : MonoBehaviour
     private void OnEnable()
     {
         options = new List<WorldInteractableOption> ();
-        menuGroup = menu.GetComponent<CanvasGroup> ();
+        canvas.enabled = false;
         InitializeOptions();
     }
 
     // Update function checks if player is in range, then updates and displays the options list.
     private void Update()
     {
-        menuGroup.alpha = 0.0f;
+        canvas.enabled = false;
 
         Vector3 dist = player.transform.position - transform.position;
         if (dist.magnitude > activationRange) return;
 
-        menu.transform.position = cam.WorldToScreenPoint(transform.position);
-        menuGroup.alpha = 1.0f;
+        canvas.enabled = true;
+        rectTransform.position = cam.WorldToScreenPoint(transform.position);
+        Debug.Log("Player is in menu range");
+
         UpdateOptions();
 
 
