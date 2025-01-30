5ff56f706b532f9c142bc113c4aa1d125c07e718
SpaceMonkeyy86 <jackreynolds9128@gmail.com>
2025-01-29 20:30:11 -0500

    Create SaveManager

---
+++ Assets/ScriptableObjects/SaveData.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveData : ScriptableObject
+{
+    public List<string> partyMembers;
+
+    /// <summary>
+    /// Pulls data from the scene to create a single serializable save object.
+    /// </summary>
+    public void InitializeSaveData()
+    {
+        partyMembers = new List<string>();
+        foreach (GhostIdentity ghost in FindObjectOfType<PartyManager>().GetGhostMajorList())
+        {
+            partyMembers.Add(ghost.name);
+        }
+    }
+
+    public void ApplySaveData()
+    {
+        // TODO
+    }
+}
