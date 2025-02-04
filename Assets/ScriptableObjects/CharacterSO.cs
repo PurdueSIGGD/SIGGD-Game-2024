97e93fcfe14c9e9109b89612020126220f8e7316
TzarofCondors <98833519+TzarofCondors@users.noreply.github.com>
2025-02-03 19:57:22 -0500

    D character scriptable object

---
+++ Assets/ScriptableObjects/CharacterSO.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/CharacterSO", order = 1)]
+public class CharacterSO : ScriptableObject
+{
+    public string charactername;
+    public Sprite sprite;
+}
