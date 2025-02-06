a58684339799416350aefca636251fdd12bbb892
Daniel Ng <ng17daniel@gmail.com>
2025-02-05 19:45:42 -0500

    hooked idol special to ghost system

--- Assets/Scripts/Ghosts/Specific Ghosts/Idol/IdolSpecial.cs
+++ Assets/Scripts/Ghosts/Specific Ghosts/Idol/IdolSpecial.cs
@@ -1,17 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 /// <summary>
 /// Special action for the idol ghost, teleports the player towards mouse position
 /// and leaves a clone that player may swap position with.
 /// </summary>
-public class IdolSpecial: MonoBehaviour, ISpecialMove
+public class IdolSpecial : MonoBehaviour, IParty, ISelectable
 {
+    bool possessing;
+    PlayerStateMachine psm;
     [SerializeField] float maxDistance = 8.0f;
     [SerializeField] float tpCoolDown = 8.0f; // cooldown for teleporting special action
     [SerializeField] float switchCoolDown = 0.5f; // cooldown for switching with clones
-    
+
     private bool isDashing; // bool for if is currently dashing
     private bool canTp = true;
     private bool canSwitch = false;
@@ -20,6 +23,31 @@ public class IdolSpecial: MonoBehaviour, ISpecialMove
 
     private GameObject idolClone; // ref to clone prefab
     private GameObject activeClone; // ref to currently active clone, if exists
+
+    public void EnterParty(GameObject player)
+    {
+        return;
+    }
+    public void ExitParty(GameObject player)
+    {
+        return;
+    }
+    public void Select(GameObject player)
+    {
+        possessing = true;
+        psm = player.GetComponent<PlayerStateMachine>();
+
+        // override delegate
+        player.GetComponent<Dash>().specialAction = HoloJump;
+    }
+    public void DeSelect(GameObject player)
+    {
+        StopAllCoroutines();
+        possessing = false;
+
+        // unoverride delegate
+        player.GetComponent<Dash>().specialAction = null;
+    }
 
     void Start()
     {
@@ -32,7 +60,7 @@ public class IdolSpecial: MonoBehaviour, ISpecialMove
     /// If none is active and the skill is not under cooldown, attempt to
     /// teleport the player towards the mouse position.
     /// </summary>
-    void OnSpecial()
+    void HoloJump()
     {
         if (canSwitch) // if there is currently a clone active, switch with it
         {
@@ -44,16 +72,16 @@ public class IdolSpecial: MonoBehaviour, ISpecialMove
         }
     }
 
-    public bool GetBool()
-    {
-        return isDashing;
-    }
-
     /// <summary>
     /// Teleports the player and creates a clone
     /// </summary>
     private IEnumerator DashCoroutine()
     {
+        print("Holo Dash!");
+
+        // instant cast time
+        psm.EnableTrigger("OPT");
+
         isDashing = true;
         canTp = false;
         dir = (mainCamera.ScreenToWorldPoint(Input.mousePosition) - transform.position).normalized;
@@ -78,7 +106,11 @@ public class IdolSpecial: MonoBehaviour, ISpecialMove
         transform.position = dest;
         isDashing = false;
 
+        // do not use psm cooldown because of swap mechanic
+        // psm.OnCooldown("c_special");
         yield return new WaitForSeconds(tpCoolDown);
+        // psm.OffCooldown("c_special");
+
         canTp = true;
     }
 
@@ -96,9 +128,16 @@ public class IdolSpecial: MonoBehaviour, ISpecialMove
         }
         else
         {
+            print("Holo Swap!");
+
+            // instant cast time
+            psm.EnableTrigger("OPT");
+
             // if so, switch places with clone
             (transform.position, activeClone.transform.position) = (activeClone.transform.position, transform.position);
+            psm.OnCooldown("c_special");
             yield return new WaitForSeconds(switchCoolDown);
+            psm.OffCooldown("c_special");
             canSwitch = true;
         }
     }
