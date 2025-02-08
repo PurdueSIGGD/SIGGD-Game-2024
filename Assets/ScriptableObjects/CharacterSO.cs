3b632d3c210be50c02d5f3b3a8ada05030667d6e
TzarofCondors <98833519+TzarofCondors@users.noreply.github.com>
2025-02-07 20:08:37 -0500

    Created character scriptable objet
    Created scriptable object for character s in dialogue

--- Assets/ScriptableObjects/CharacterSO.cs
+++ Assets/ScriptableObjects/CharacterSO.cs
@@ -6,5 +6,14 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/CharacterSO", order = 1)]
 public class CharacterSO : ScriptableObject
 {
-    public string charactername;
+    public string characterName;
+
+    [System.Serializable]
+    public class ExpressionList
+    {
+        public string expressionName;
+        public Sprite expressionSprite;
+    }
+
+    public List<ExpressionList> expressionList = new List<ExpressionList>();
 }
