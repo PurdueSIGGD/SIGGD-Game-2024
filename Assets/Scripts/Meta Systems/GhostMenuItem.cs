fa35234547e81d7284a6f2ed830c89c0dcda6bac
DinoSage <dinosage5632@gmail.com>
2025-03-03 17:23:23 -0500

    [%] Move and Reorganized some files

---
+++ Assets/Scripts/Meta Systems/GhostMenuItem.cs
@@ -0,0 +1,39 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GhostMenuItem : MonoBehaviour
+{
+    [SerializeField] public TextMeshProUGUI textComponent;
+    [SerializeField] public Image imageComponent;
+    [SerializeField] public Button buttonComponent;
+
+    public GhostIdentity identity;
+    public PartyEditor menu;
+
+    void Start()
+    {
+        textComponent.SetText(identity.name);
+        buttonComponent.onClick.AddListener(() =>
+        {
+            menu.Select(this);
+        });
+    }
+
+    public void SetSelected(bool selected)
+    {
+        if (selected)
+        {
+            imageComponent.color = Color.green;
+        }
+        else
+        {
+            imageComponent.color = Color.gray;
+        }
+    }
+
+    public void UISelect()
+    {
+        menu.Select(this);
+    }
+}
--- Assets/Scripts/StatManager.cs
+++ Assets/Scripts/StatManager.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
-using static GhostBuff;
 
 [DisallowMultipleComponent]
 public class StatManager : MonoBehaviour, IStatList
