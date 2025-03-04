a8b9e1f38d43ef155ceabbe7f2fee65c84284f21
xinmew <60266027+WangXin1507@users.noreply.github.com>
2025-03-03 21:00:35 -0500

    added automatic screen shake to heavy attack

--- Assets/PlayerParticles.cs
+++ Assets/PlayerParticles.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class PlayerParticles : MonoBehaviour
 {
+    private CameraShake shake;
+
     [SerializeField] ParticleSystem heavyCharging;
     [SerializeField] ParticleSystem heavyPrimed;
     [SerializeField] ParticleSystem heavyAttack;
+
+    void Start()
+    {
+        shake = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraShake>();
+    }
 
     public void StartHeavyChargeUp()
     {
@@ -26,6 +33,13 @@ public class PlayerParticles : MonoBehaviour
     public void StopHeavyPrimed()
     {
         heavyPrimed.Stop();
+        StartCoroutine(HeavyAttackCoroutine());
+    }
+
+    private IEnumerator HeavyAttackCoroutine()
+    {
+        yield return new WaitForSeconds(0.3f);
         heavyAttack.Play();
+        shake.Shake(0.2f, 10f, 0, 10, new Vector2(Random.Range(-0.5f, 0.5f), 1f));
     }
 }
