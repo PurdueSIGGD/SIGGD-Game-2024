65a9d920b61cfa70e2d572349b53aee83a9f7226
jaldridge <89814068+jaldridg@users.noreply.github.com>
2025-03-13 01:04:47 -0400

    Beginning AudioMixer mega test

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour {
@@ -9,27 +10,43 @@ public class AudioManager : MonoBehaviour {
 
     // SOUNDTRACK
     [SerializeField] MusicTrack japanTheme;
+    [SerializeField] MusicTrack seamstressTheme;
     
     public enum MusicTrackName {
-        JAPAN // loopStart = 21.943; loopEnd = 197.486  
+        //                  loopStart       loopEnd
+        JAPAN, //           21.943          197.486
+        SEAMSTRESS //       11.912          83.383    
     }
 
     // SFX
-    [SerializeField] AudioTrack jump;
-    [SerializeField] AudioTrack lightAttack;
-    [SerializeField] AudioTrack footstep;
+    [SerializeField] AudioTrack jumpSFX;
+    [SerializeField] AudioTrack lightAttackSFX;
+    [SerializeField] AudioTrack footstepSFX;
     public enum SFXTrackName {
         JUMP,
         LIGHT_ATTACK,
         FOOTSTEP
     }
 
-    private MusicTrackName currentTrackName;
-    // Start is called before the first frame update
+    [SerializeField] AudioTrack britishAnt;
 
+    public enum DialogueTrackName {
+        BRITISH_ANT // If this is still here, remove it
+    }
+
+    private MusicTrackName currentTrackName;
+
+    // Energy level: This variable is set by other scripts to manage the tracks energy levels
+    // 0.0 to 1.0, low energy to high energy
+    // Track 1 plays when energy is between 0.0 and 0.5 with greatest volume at energy = 0.0
+    // Track 2 always plays and it's volume is greatest when energy = 0.5
+    // Track 3 plays when energy is between 0.5 and 1.0 with greatest volume at energy = 1.0
+    [SerializeField] float energyLevel;
+
+    // Start is called before the first frame update
     void Start() {
         currentTrackName = MusicTrackName.JAPAN;
-        getCurrentTrack(currentTrackName).PlayTrack();
+        GetCurrentTrack().PlayTrack();
     }
 
     // Update is called once per frame
@@ -38,33 +55,85 @@ public class AudioManager : MonoBehaviour {
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)) {
             tempStepCounter += Time.deltaTime;
             if (tempStepCounter > 0.4f) {
-                footstep.PlayTrack();
+                footstepSFX.PlayTrack();
                 tempStepCounter = 0.0f;
             }
         }   
 
         if (Input.GetKeyDown(KeyCode.Space)) {
-            jump.PlayTrack();
+            jumpSFX.PlayTrack();
         }
 
         if (Input.GetKeyDown(KeyCode.E)) {
-            lightAttack.PlayTrack();
+            lightAttackSFX.PlayTrack();
+        }
+
+        if (Input.GetKeyDown(KeyCode.B)) {
+            britishAnt.PlayTrack();
+        }
+
+        if (Input.GetKeyDown(KeyCode.C)) {
+            MusicTrackName nextTrack = currentTrackName == MusicTrackName.JAPAN ? MusicTrackName.SEAMSTRESS : MusicTrackName.JAPAN;
+            StartCoroutine(Crossfade(nextTrack, 10.0f));
         }
     }
 
     // Swaps the current track with no crossfade
-    public void changeCurrentTrack(MusicTrackName newTrack) {
-        getCurrentTrack(currentTrackName).StopTrack();
+    public void ChangeCurrentTrack(MusicTrackName newTrack) {
+        GetCurrentTrack().StopTrack();
         currentTrackName = newTrack;
-        getCurrentTrack(currentTrackName).PlayTrack();
+        GetCurrentTrack().PlayTrack();
     }
 
-    private MusicTrack getCurrentTrack(MusicTrackName trackName) {
-        switch (trackName) {
+    public MusicTrack GetCurrentTrack() {
+        switch (currentTrackName) {
             case MusicTrackName.JAPAN:
                 return japanTheme;
+            case MusicTrackName.SEAMSTRESS:
+                return seamstressTheme;
             default:
                 return null;
         }
+    }
+
+    public MusicTrack GetMusicTrack(MusicTrackName trackName) {
+        switch (trackName) {
+            case MusicTrackName.JAPAN:
+                return japanTheme;
+            case MusicTrackName.SEAMSTRESS:
+                return seamstressTheme;
+            default:
+                return null;
+        }
+    }
+
+    // Used by MusicTracks to find out which track energy levels to play
+    public float GetEnergyLevel() { return energyLevel; }
+
+    // Used by tracks outside of audio to set the ~mood~
+    public void SetEnergyLevel(float newLevel) { energyLevel = newLevel; }
+
+    // Fades into the given track over fadeTime seconds
+    public IEnumerator Crossfade(MusicTrackName trackName, float fadeTime) {
+        if (fadeTime <= 0) {
+            ChangeCurrentTrack(trackName);
+            yield return null;
+        }
+
+        int fadeSteps = 20;
+        float stepTime = fadeTime / fadeSteps;
+        MusicTrack originalTrack = GetMusicTrack(currentTrackName);
+        MusicTrack newTrack = GetMusicTrack(trackName);
+
+        // Fade by adjusting volume over multiple steps
+        newTrack.PlayTrack();  
+        for (int i = 0; i < fadeSteps; i++) {
+            float newVolume = 1.0f / fadeSteps * i;
+            originalTrack.SetTrackVolume(1.0f - newVolume);
+            newTrack.SetTrackVolume(newVolume);            
+            yield return new WaitForSeconds(stepTime);
+        }
+        originalTrack.StopTrack();
+        currentTrackName = trackName;
     }
 }
--- Assets/Scripts/Audio/MusicTrack.cs
+++ Assets/Scripts/Audio/MusicTrack.cs
@@ -1,20 +1,26 @@
 using System;
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 // Attached to music which can be looped
+// Supports varying intensity levels
 public class MusicTrack : MonoBehaviour
 {
     // The timestamps of the loop points in seconds
     [SerializeField] float loopStart;
     [SerializeField] float loopEnd;
+
+    // The loudest volume which can be reached by the level tracks
+    // The tracks are normalized around this value 
+    private float maxVolume = 1.0f;
     
     // The index of the next track to play
     // When 0, plays tracks 0, 2, and 4
     // When 1, plays tracks 1, 3, and 5
     private int currentTrackOffset = 0;
-    private const int LEVEL_TWO_TRACK_OFFSET = 2;
-    private const int LEVEL_THREE_TRACK_OFFSET = 4;
+    private const int LEVEL_TWO_TRACK_OFFSET = 2;   // Shifts to level 2 tracks
+    private const int LEVEL_THREE_TRACK_OFFSET = 4; // Shifts to level 3 tracks
 
     // An array of all tracks
     // index 0: Level 1, copy 1
@@ -25,12 +31,10 @@ public class MusicTrack : MonoBehaviour
     // index 5: Level 3, copy 2
     [SerializeField] AudioSource[] tracks;
 
-    // 0.0 to 1.0, low energy to high energy
-    // Track 1 plays when energy is between 0.0 and 0.5 with greatest volume at energy = 0.0
-    // Track 2 always plays and it's volume is greatest when energy = 0.5
-    // Track 3 plays when energy is between 0.5 and 1.0 with greatest volume at energy = 1.0
-    [SerializeField] float energyLevel;
+    // Used to manage which tracks to play
     private bool isHighEnergy = false;
+
+    private bool isPlaying = false;
 
     void Start() { 
         // If loopEnd is the same as the end of the track, adjust it
@@ -39,36 +43,42 @@ public class MusicTrack : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
+    // Update is called once per frame 
     void Update() {
-        // Tests if the energy level went from above to below high energy or vice versa
-        // If so, we need to flip between tracks 1 and 3
-        bool didEnergySwap = energyLevel < 0.5 == isHighEnergy;
-        isHighEnergy = energyLevel > 0.5;
-        // Keep the music level on par with the player's experience
-        // This will probably be a variable external to the audio system later
-        // But for now (and testing), it's in this class
-        if (didEnergySwap) {
-            // Flipped from low to high energy
-            if (isHighEnergy) {
-                tracks[currentTrackOffset].Stop();
-                tracks[LEVEL_THREE_TRACK_OFFSET + currentTrackOffset].Play();
-                tracks[LEVEL_THREE_TRACK_OFFSET + currentTrackOffset].time = tracks[LEVEL_TWO_TRACK_OFFSET + currentTrackOffset].time;
+        if (isPlaying) {
+            // Tests if the energy level went from above to below high energy or vice versa
+            // If so, we need to flip between tracks 1 and 3
+            AudioManager am = GetComponentInParent<AudioManager>();
+            float energyLevel = am.GetEnergyLevel();
+            bool didEnergySwap = energyLevel < 0.5 == isHighEnergy;
+            isHighEnergy = energyLevel > 0.5;
+            // Keep the music level on par with the player's experience
+            // This will probably be a variable external to the audio system later
+            // But for now (and testing), it's in this class
+            if (didEnergySwap) {
+                // Flipped from low to high energy
+                if (isHighEnergy) {
+                    tracks[currentTrackOffset].Stop();
+                    tracks[LEVEL_THREE_TRACK_OFFSET + currentTrackOffset].Play();
+                    tracks[LEVEL_THREE_TRACK_OFFSET + currentTrackOffset].time = tracks[LEVEL_TWO_TRACK_OFFSET + currentTrackOffset].time;
 
-            // Flipped from high to low energy
-            } else {
-                tracks[LEVEL_THREE_TRACK_OFFSET + currentTrackOffset].Stop();
-                tracks[currentTrackOffset].Play();
-                tracks[currentTrackOffset].time = tracks[LEVEL_TWO_TRACK_OFFSET + currentTrackOffset].time;
+                // Flipped from high to low energy
+                } else {
+                    tracks[LEVEL_THREE_TRACK_OFFSET + currentTrackOffset].Stop();
+                    tracks[currentTrackOffset].Play();
+                    tracks[currentTrackOffset].time = tracks[LEVEL_TWO_TRACK_OFFSET + currentTrackOffset].time;
+                }
             }
+            AdjustLeveledTrackVolumes();
         }
-        setTrackVolumes();
     }
 
     public void PlayTrack() {
+        AudioManager am = GetComponentInParent<AudioManager>();
         // Play base level 2 (since it always plays)
         tracks[LEVEL_TWO_TRACK_OFFSET].Play();
         // Play level 1 or 3 based on current energy level
+        float energyLevel = am.GetEnergyLevel();
         if (energyLevel < 0.5) {
             tracks[currentTrackOffset].Play();
             isHighEnergy = false;
@@ -76,31 +86,41 @@ public class MusicTrack : MonoBehaviour
             tracks[LEVEL_THREE_TRACK_OFFSET + currentTrackOffset].Play();
             isHighEnergy = true;
         }
-        setTrackVolumes();
+        isPlaying = true;
+        AdjustLeveledTrackVolumes();
 
         currentTrackOffset = 0;
-        StartCoroutine(autoLoop());
+        StartCoroutine(AutoLoop());
     }
 
     public void StopTrack() {
         for (int i = 0; i < tracks.Length; i++) {
             tracks[i].Stop();
         }
-        StopCoroutine(autoLoop());
+        isPlaying = false;
+        StopCoroutine(AutoLoop());
+    }
+ 
+    public void SetTrackVolume(float volume) {
+        maxVolume = volume;
+        AdjustLeveledTrackVolumes();
     }
 
     // Sets track volumes to match energy levels
-    private void setTrackVolumes() {
-        float levelTwoTrackVolume = 1.0f - Math.Abs(energyLevel - 0.5f) * 2;
+    private void AdjustLeveledTrackVolumes() {
+        AudioManager am = GetComponentInParent<AudioManager>();
+        // Calculates the volume of the level two track, normalizing around the maxVolume value
+        float levelTwoTrackVolume = maxVolume - Math.Abs(am.GetEnergyLevel() - (maxVolume / 2)) * 2;
+        float levelOneOrThreeVolume = maxVolume - levelTwoTrackVolume;
         if (isHighEnergy) {
-            tracks[LEVEL_THREE_TRACK_OFFSET + currentTrackOffset].volume = 1.0f - levelTwoTrackVolume;
+            tracks[LEVEL_THREE_TRACK_OFFSET + currentTrackOffset].volume = levelOneOrThreeVolume;
         } else {
-            tracks[currentTrackOffset].volume = 1.0f - levelTwoTrackVolume;
+            tracks[currentTrackOffset].volume = levelOneOrThreeVolume;
         }
         tracks[LEVEL_TWO_TRACK_OFFSET + currentTrackOffset].volume = levelTwoTrackVolume;
     }
 
-    private IEnumerator autoLoop() {
+    private IEnumerator AutoLoop() {
         float trackPlaytime = loopEnd - tracks[LEVEL_TWO_TRACK_OFFSET + currentTrackOffset].time;
         // Wait most of the track
         float trackMajorityLength = trackPlaytime * 0.98f;
@@ -115,12 +135,12 @@ public class MusicTrack : MonoBehaviour
             // Play the right tracks based on energy level and set playback to loopStart
             tracks[LEVEL_TWO_TRACK_OFFSET + currentTrackOffset].Play();
             tracks[LEVEL_TWO_TRACK_OFFSET + currentTrackOffset].time = loopStart;
-            if (!isHighEnergy) {
-                tracks[currentTrackOffset].Play();
-                tracks[currentTrackOffset].time = loopStart;
-            } else {
+            if (isHighEnergy) {
                 tracks[LEVEL_THREE_TRACK_OFFSET + currentTrackOffset].Play();
                 tracks[LEVEL_THREE_TRACK_OFFSET + currentTrackOffset].time = loopStart;
+            } else {
+                tracks[currentTrackOffset].Play();
+                tracks[currentTrackOffset].time = loopStart;
             }
 
             trackMajorityLength = (loopEnd - tracks[LEVEL_TWO_TRACK_OFFSET + currentTrackOffset].time) * 0.98f;
