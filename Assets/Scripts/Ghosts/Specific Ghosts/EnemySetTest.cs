ef9fc79774ee4173f3d9179fbf9fefbf465f5736
xinmew <60266027+WangXin1507@users.noreply.github.com>
2025-03-17 20:12:32 -0700

    testing chaining enemies for yume's special
    do not try to run, ur unity client will crash.

---
+++ Assets/Scripts/Ghosts/Specific Ghosts/EnemySetTest.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections.LowLevel.Unsafe;
+using UnityEngine;
+
+/// <summary>
+/// Placeholder class for testing yume speical ability
+/// Expect to be replaced after enemy spawn is implemented
+/// </summary>
+[DisallowMultipleComponent]
+public class EnemySetTest : MonoBehaviour
+{
+    public static EnemySetTest Instance;
+    public static Queue<GameObject> enemies = new Queue<GameObject>();
+    void Start()
+    {
+        Instance = this;
+        foreach (GameObject gameObject in GameObject.FindGameObjectsWithTag("Enemy"))
+        {
+            enemies.Enqueue(gameObject);
+            Debug.Log(gameObject.name);
+        }
+    }
+}
---
+++ Assets/Scripts/Ghosts/Specific Ghosts/YumeSpecial.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class YumeSpecial : MonoBehaviour
+{
+    // TODO remove
+    private Queue<GameObject> enemies;
+    private Queue<GameObject> hitEnemies = new Queue<GameObject>();
+    private node ptr;
+    
+    // TODO remove
+    class node
+    {
+        public GameObject enemy;
+        public node next;
+
+        public node(GameObject enemy, node next)
+        {
+            this.enemy = enemy;
+            this.next = next;
+        }
+
+        public node(GameObject enemy)
+        {
+            this.enemy = enemy;
+            this.next = null;
+        }
+
+        public node() 
+        {
+            enemy = null;
+            next = null;
+        }
+    }
+
+    void Start()
+    {
+        ptr = new node();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.V))
+        {
+            ChainAllEnemies();
+            while (ptr.next != null)
+            {
+                Debug.Log(ptr.enemy.name);
+            }
+        }
+    }
+
+    public void ChainAllEnemies()
+    {
+        ptr.enemy = EnemySetTest.enemies.Dequeue();
+        Debug.Log(ptr.enemy.name);
+        ptr.next = ChainNextEnemy(ptr);
+    }
+
+    private node ChainNextEnemy(node cur)
+    {
+        Debug.Log(EnemySetTest.enemies.Count);
+
+        node n = new node();
+        float minDist = float.MaxValue;
+        for (int i = 0; i < EnemySetTest.enemies.Count; i++)
+        {
+            GameObject enemy = EnemySetTest.enemies.Dequeue();
+
+            if (enemy.GetInstanceID() == cur.enemy.GetInstanceID()) // if checking the currently linked enemy, pass
+            {
+                continue;
+            }
+
+            float dist = Vector2.Distance(cur.enemy.transform.position, enemy.transform.position); // cur must have an enemy
+
+            if (dist < minDist)
+            {
+                minDist = dist;
+                n.enemy = enemy;
+            }
+
+            EnemySetTest.enemies.Enqueue(enemy);
+        }
+
+        if (n.enemy == null) // and other condtions
+        {
+            return null; // end of the chain
+        }
+
+        n.next = ChainNextEnemy(n); // else continue the chain
+
+        return n;
+    }
+}
