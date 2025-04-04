c95d4e19eddc08c6f89aab77f27f5b17247bf734
WangXin1507 <simonwang421@outlook.com>
2025-04-04 18:18:23 -0400

    created blight particles and silas prefab

--- Assets/Scripts/Ghosts/Specific Ghosts/PlagueDoctor/PlagueDoctorSpecial.cs
+++ Assets/Scripts/Ghosts/Specific Ghosts/PlagueDoctor/PlagueDoctorSpecial.cs
@@ -5,14 +5,7 @@ using UnityEngine;
 
 public class PlagueDoctorSpecial : MonoBehaviour
 {
-
-    [SerializeField] GameObject blightPotion;
-    [SerializeField] float initialSpeed;
-    [SerializeField] Vector2 bottleOffset;
-
-    // TODO, replace these when assembling ghost
-    [SerializeField] float cooldown;
-    private float curCd;
+    [HideInInspector] public SilasManager manager;
 
     private Camera mainCamera;
 
@@ -22,26 +15,18 @@ public class PlagueDoctorSpecial : MonoBehaviour
         mainCamera = Camera.main;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        curCd -= Time.deltaTime;
-    }
-
     void StartDash()
     {
-        if (curCd > 0) { return; }
-        GameObject bottle = Instantiate(blightPotion, (Vector2)transform.position + bottleOffset, transform.rotation);
+        GameObject bottle = Instantiate(manager.blightPotion, (Vector2)transform.position + manager.bottleOffset, transform.rotation);
         Vector2 mousePos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
         Vector2 playerPos = PlayerID.instance.transform.position;
-        bottle.GetComponent<Rigidbody2D>().velocity = (mousePos - playerPos).normalized * initialSpeed;
-        PlayerStateMachine psm = this.GetComponent<PlayerStateMachine>();
+        bottle.GetComponent<Rigidbody2D>().velocity = (mousePos - playerPos).normalized * manager.initialSpeed;
+        PlayerStateMachine psm = GetComponent<PlayerStateMachine>();
         psm.EnableTrigger("OPT");
-        curCd = cooldown;
     }
 
     private void OnDrawGizmos()
     {
-        Gizmos.DrawWireSphere((Vector2)transform.position + bottleOffset, 0.3f);
+        Gizmos.DrawWireSphere((Vector2)transform.position + manager.bottleOffset, 0.3f);
     }
 }
