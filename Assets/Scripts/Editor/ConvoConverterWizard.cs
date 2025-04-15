84a8913179f5b5b552bff9514fa8e154d6ffad03
DinoSage <dinosage5632@gmail.com>
2025-04-14 21:28:33 -0400

    [+] Wizard for Convo SO

---
+++ Assets/Scripts/Editor/ConvoConverterWizard.cs
@@ -0,0 +1,38 @@
+using EditorUtils;
+using UnityEditor;
+using UnityEngine;
+
+public class ConvoConverterWizard : ScriptableWizard
+{
+    public FolderReference targetFolder;
+    public TextAsset[] convoFiles;
+
+    [MenuItem("SIGGD/Convo Recipe Wizard")]
+    static void CreateWizard()
+    {
+        ScriptableWizard.DisplayWizard<ConvoConverterWizard>("Convo Recipe Creator", "Create Convos", "Clear");
+    }
+
+    void OnWizardCreate()
+    {
+        foreach (var convoFile in convoFiles)
+        {
+            ConvoData data = new ConvoData();
+            EditorJsonUtility.FromJsonOverwrite(convoFile.ToString(), data);
+
+            Debug.Log(convoFile.name);
+            Debug.Log(data.convoName);
+
+            ConvoSO so = ScriptableObject.CreateInstance<ConvoSO>();
+            so.data = data;
+
+            AssetDatabase.CreateAsset(so, $"{targetFolder.Path}/{data.convoName}.asset");
+        }
+    }
+
+    void OnWizardOtherButton()
+    {
+        convoFiles = null;
+    }
+
+}
