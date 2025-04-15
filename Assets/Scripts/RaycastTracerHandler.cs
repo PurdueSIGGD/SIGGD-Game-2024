128ade54a9dc10cfd83a5a28ae47b65e8e3c67c7
Nathan Whitacre <whitacre@purdue.edu>
2025-04-15 14:16:00 -0400

    Gave bullet tracer vfx travel time
    THIS IS SO JUICY AHHHH

--- Assets/Scripts/RaycastTracerHandler.cs
+++ Assets/Scripts/RaycastTracerHandler.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class RaycastTracerHandler : MonoBehaviour
 {
+    [SerializeField] private float extentionDurationTime;
+    [SerializeField] private float travelSpeed;
     [SerializeField] private float fadeOutDelayTime;
     [SerializeField] private float fadeOutDurationTime;
 
@@ -27,9 +29,25 @@ public class RaycastTracerHandler : MonoBehaviour
         lineRenderer.enabled = true;
         lineRenderer.startColor = startColor;
         lineRenderer.endColor = endColor;
-        lineRenderer.SetPosition(0, startPosition);
-        lineRenderer.SetPosition(1, endPosition);
+        //lineRenderer.SetPosition(0, startPosition);
+        //lineRenderer.SetPosition(1, endPosition);
+        StartCoroutine(extendTracer(startPosition, endPosition));
         StartCoroutine(fadeOutTracer());
+    }
+
+    private IEnumerator extendTracer(Vector2 startPosition, Vector2 endPosition)
+    {
+        float distance = Vector2.Distance(startPosition, endPosition);
+        float travelTime = distance / travelSpeed;
+        lineRenderer.SetPosition(0, startPosition);
+        lineRenderer.SetPosition(1, startPosition);
+        int step = 20;
+        for (int i = 0; i < step; i++)
+        {
+            lineRenderer.SetPosition(1, Vector2.Lerp(startPosition, endPosition, (float) i / (float) step));
+            yield return new WaitForSeconds(travelTime / (float) step);
+        }
+        lineRenderer.SetPosition(1, endPosition);
     }
 
     private IEnumerator fadeOutTracer()
