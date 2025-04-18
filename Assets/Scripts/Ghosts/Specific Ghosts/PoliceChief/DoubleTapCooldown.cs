889e74089f4fd67dfa9b9cd0b6ad7e813bf4f442
TzarofCondors <98833519+TzarofCondors@users.noreply.github.com>
2025-04-18 19:27:47 -0400

    D-North-Double-Tap
    File for double tap script is made

---
+++ Assets/Scripts/Ghosts/Specific Ghosts/PoliceChief/DoubleTapCooldown.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField] ScriptableObject doubleTapSkill;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
