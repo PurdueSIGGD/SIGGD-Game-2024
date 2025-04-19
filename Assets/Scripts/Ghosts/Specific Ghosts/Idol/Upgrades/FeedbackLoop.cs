e30fd88ba4875c62dbe97d688017fa6f23644f9a
Daniel Ng <ng17daniel@gmail.com>
2025-04-18 20:13:17 -0400

    eva skill script

---
+++ Assets/Scripts/Ghosts/Specific Ghosts/Idol/Upgrades/FeedbackLoop.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FeedbackLoop : Skill
+{
+    StatManager stats;
+    List<int> values = new List<int>
+    {
+        0, 7, 14, 21, 28
+    };
+    int percentIncrease = 0;
+    void Start()
+    {
+        stats = gameObject.GetComponent<StatManager>();
+    }
+    public override void AddPointTrigger()
+    {
+        UpdateSkill();
+    }
+
+    public override void ClearPointsTrigger()
+    {
+        UpdateSkill();
+    }
+
+    public override void RemovePointTrigger()
+    {
+        UpdateSkill();
+    }
+    /// <summary>
+    /// Removes the old modifier and applies the new modifier.
+    /// </summary>
+    private void UpdateSkill()
+    {
+        stats.ModifyStat("Special Cooldown", -percentIncrease);
+        percentIncrease = values[skillPts];
+        stats.ModifyStat("Special Cooldown", percentIncrease);
+    }
+}
