a932ca16b934c0c4d449f46a8cea76a0ed636078
aayushrh <73722373+aayushrh@users.noreply.github.com>
2025-04-19 18:57:15 -0400

    Overcharge

--- Assets/Scripts/Ghosts/Specific Ghosts/PoliceChief/PoliceChiefOvercharged.cs
+++ Assets/Scripts/Ghosts/Specific Ghosts/PoliceChief/PoliceChiefOvercharged.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class PoliceChiefOvercharged : Skill
@@ -7,9 +8,27 @@ public class PoliceChiefOvercharged : Skill
     [SerializeField] int[] pointCounts = {0, 40, 80, 120, 160};
     private int bonusDamage = 0;
     private StatManager statManager;
+    private PlayerStateMachine playerStateMachine;
+    private float timer = 0.0f;
 
     private void Start()
     {
+        statManager = PlayerID.instance.GetComponent<StatManager>();
+        playerStateMachine = PlayerID.instance.GetComponent<PlayerStateMachine>();
+    }
+
+    private void Update()
+    {
+        if (timer > 0.0f) {
+            timer -= Time.deltaTime;
+            GetComponent<Animator>().SetTrigger("ToIdle");
+            Debug.Log("Explode");
+        }
+
+        if (playerStateMachine.currentAnimation.Equals("police_chief_special_primed"))
+        {
+            timer = 2f;
+        }
     }
 
     public override void AddPointTrigger()
