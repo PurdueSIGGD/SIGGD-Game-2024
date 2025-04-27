74633679b2684f88894f31727ad37defb731f854
Daniel Ng <ng17daniel@gmail.com>
2025-04-27 17:10:40 -0400

    roomba bugfix, cleanup and polish

--- Assets/Scripts/Enemies/Roomba.cs
+++ Assets/Scripts/Enemies/Roomba.cs
@@ -11,10 +11,28 @@ public class Roomba : EnemyStateManager
     [Header("Self Detonate")]
     [SerializeField] protected Transform kaboomTrigger;
     [SerializeField] protected DamageContext kaboomDamage;
+    [SerializeField] float damage;
+    [SerializeField] GameObject explodeMarker;
+    [SerializeField] GameObject explodeVisual;
+    SpriteRenderer spriteRenderer;
+    bool toggleRed = true;
 
+    void Start()
+    {
+        kaboomDamage.damage = damage;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    protected void OnInitiateKaboom()
+    {
+        StartCoroutine(flicker());
+    }
     // Check for player in blast radius and do damage
     protected void OnKaboomEvent()
     {
+        explodeMarker.SetActive(false);
+        explodeVisual.SetActive(true);
+        StopAllCoroutines();
         GenerateDamageFrame(kaboomTrigger.position, kaboomTrigger.lossyScale.x, kaboomDamage, gameObject);
     }
 
@@ -22,8 +40,7 @@ public class Roomba : EnemyStateManager
     {
         base.OnFinishAnimation();
         kaboomDamage.victim = this.gameObject;
-        GameplayEventHolder.OnDeath?.Invoke(kaboomDamage);
-        Destroy(gameObject);
+        GetComponent<Health>().Damage(kaboomDamage, this.gameObject);
     }
 
     // Draws the Enemy attack range in the editor
@@ -31,5 +48,24 @@ public class Roomba : EnemyStateManager
     {
         base.OnDrawGizmos();
         Gizmos.DrawWireSphere(kaboomTrigger.position, kaboomTrigger.lossyScale.x);
+    }
+    IEnumerator flicker()
+    {
+        while (true)
+        {
+            explodeMarker.SetActive(toggleRed);
+            if (toggleRed)
+            {
+                spriteRenderer.color = Color.red;
+                toggleRed = false;
+            }
+            else
+            {
+                spriteRenderer.color = Color.white;
+                toggleRed = true;
+            }
+            yield return new WaitForSeconds(0.05f);
+            yield return null;
+        }
     }
 }
