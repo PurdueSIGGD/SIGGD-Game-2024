6b82c5e13f5e2c695a5627810483d7267359ba70
TzarofCondors <98833519+TzarofCondors@users.noreply.github.com>
2025-04-29 01:23:06 -0400

    deleted unnecessary stuff

--- Assets/Scripts/Player/OrionManager.cs
+++ Assets/Scripts/Player/OrionManager.cs
@@ -26,14 +26,4 @@ public class OrionManager : GhostManager
         }
         isDashEnabled = (!(getSpecialCooldown() > 0f  || isAirbornePostDash));
     }
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
-        {
-            if (collision.transform.position.y < transform.position.y)
-            {
-                AudioManager.Instance.SFXBranch.PlaySFXTrack(SFXTrackName.LANDING);
-            }
-        }
-    }
 }
