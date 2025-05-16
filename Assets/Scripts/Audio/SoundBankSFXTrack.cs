8a87652bd1845f6f7ff8ab8b689f005b04fb9c7b
WangXin1507 <simonwang421@outlook.com>
2025-05-15 22:47:38 -0400

    now able to correctly create audio component for voice lines, conversations, and various types of SFX

--- Assets/Scripts/Audio/SoundBankSFXTrack.cs
+++ Assets/Scripts/Audio/SoundBankSFXTrack.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class SoundBankSFXTrack : AbstractSoundBank, ISFXTrack {
 
     // The predetermined pitch values which define the working pitch range of the sound effect
-    [SerializeField] private float minPitch;
-    [SerializeField] private float maxPitch;
+    public float minPitch = 1;
+    public float maxPitch = 1;
     
     public void SetPitch(float currentValue, float maxValue) {
         foreach (var sound in sounds) {
--- Assets/Scripts/Meta Systems/Audio/AudioWizard.cs
+++ Assets/Scripts/Meta Systems/Audio/AudioWizard.cs
@@ -1,21 +1,31 @@
 #if UNITY_EDITOR
 
+using System;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class AudioWizard : ScriptableWizard
 {
     [SerializeField] string audioName;
     [SerializeField] AudioType audioType;
     [SerializeField] TrackType trackType;
-    [SerializeField] AudioClip[] clips;
+    [SerializeField] Mixer mixerType;
+    [SerializeField] string sourcePath = "";
+    [SerializeField] List<AudioClip> clips;
+
+    private AudioMixer audioMixer;
+
+    // used for updating wizard ui
+    private AudioType oldAudioType = AudioType.VA;
+    private string oldPath = "";
 
     enum AudioType
     {
-        Music,
         VA,
-        SFX
+        SFX,
+        Conversation
     }
 
     enum TrackType
@@ -24,6 +34,23 @@ public class AudioWizard : ScriptableWizard
         Looping,
         SoundBank,
         Leveled
+    }
+
+    enum Mixer
+    {
+        VA,
+        AmbientSFX,
+        BIGSFX,
+        DefaultSFX,
+        Master,
+        Music,
+        PrioritySFX,
+        SFX
+    }
+
+    private void OnEnable()
+    {
+        audioMixer = AssetDatabase.LoadAssetAtPath("Assets/Audio/Mixer.mixer", typeof(AudioMixer)) as AudioMixer;
     }
 
     [MenuItem("SIGGD/Create New Audio")]
@@ -40,30 +67,83 @@ public class AudioWizard : ScriptableWizard
         // load the AudioManager prefab
         GameObject audioManager = PrefabUtility.LoadPrefabContents("Assets/Prefabs/Meta Systems/Audio Manager.prefab");
 
-        Transform parent = audioManager.transform.Find("Dialogue");
+        Transform parent = FindParent(audioManager);
+        GameObject audioComp = new(audioName);
+        audioComp.transform.parent = parent;
 
-        GameObject newDialogue = new GameObject(audioName);
-        newDialogue.transform.parent = parent;
+        // ok below should be some brilliant code that just generically fill up the audio component
+        // BUT each audio and track type is like created so diffferently
+        // that it doesn't feel worth it at this point.
+        // If you are reading this (why?? hopefully not because my code is throwing a trillion errors)
+        // and feel u can fix it, by all means give it a try, thanks xoxo
 
-        ConversationAudioHolder conversationAudioHolder = newDialogue.AddComponent<ConversationAudioHolder>();
-        conversationAudioHolder.tracks = new List<OneShotVATrack>();
+        // ok disclaimers aside, I'm gonna start writing this monstrosity now god help me
 
-        foreach (AudioClip clip in clips)
+        switch (audioType)
         {
-            AudioSource source = newDialogue.AddComponent<AudioSource>();
-            OneShotVATrack oneShotVATrack = newDialogue.AddComponent<OneShotVATrack>();
-
-            source.clip = clip;
-            source.playOnAwake = false;
-
-            oneShotVATrack.track = source;
-            conversationAudioHolder.tracks.Add(oneShotVATrack);
+            case AudioType.VA:
+                CreateVA(audioComp);
+                break;
+            case AudioType.SFX:
+                CreateSFX(audioComp);
+                break;
+            case AudioType.Conversation:
+                CreateConversation(audioComp);
+                break;
+            default:
+                Debug.LogWarning("code fell through while creating audio holder, aborting save");
+                return;
         }
 
 
         // save the AudioManager prefab
         PrefabUtility.SaveAsPrefabAsset(audioManager, "Assets/Prefabs/Meta Systems/Audio Manager.prefab");
     }
+
+    private void OnWizardUpdate()
+    {
+        // if the audio type has changed, make corresponding changes to default set up
+        if (oldAudioType != audioType)
+        {
+            oldAudioType = audioType;
+            if (audioType == AudioType.VA)
+            {
+                mixerType = Mixer.VA;
+                trackType = TrackType.SoundBank;
+            }
+            if (audioType == AudioType.Conversation)
+            {
+                mixerType = Mixer.VA;
+                trackType = TrackType.OneShot;
+            }
+        }
+
+        // if the source path has changed, attempt to read it
+        if (!sourcePath.Equals(oldPath))
+        {
+            oldPath = sourcePath;
+            String[] loadedGUID = AssetDatabase.FindAssets("", new[] { sourcePath });
+
+            if (loadedGUID.Length > 0 && clips == null)
+            {
+                clips = new List<AudioClip>();
+            }
+
+            foreach (string guid in loadedGUID)
+            {
+                String path = AssetDatabase.GUIDToAssetPath(guid);
+                AudioClip clip = AssetDatabase.LoadAssetAtPath(path, typeof(AudioClip)) as AudioClip;
+                clips.Add(clip);
+            }
+        }
+    }
+
+
+
+
+    // =================
+    // helper functions
+    // =================
 
     private bool ValidInputs()
     {
@@ -72,12 +152,142 @@ public class AudioWizard : ScriptableWizard
             Debug.LogWarning("Name field must not be empty when adding audio");
             return false;
         }
-        if (clips.Length == 0)
+        if (clips.Count == 0)
         {
             Debug.LogWarning("You must add at least one audio clip");
             return false;
         }
         return true;
+    }
+
+    private Transform FindParent(GameObject audioManager)
+    {
+        switch (audioType)
+        {
+            case AudioType.VA:
+                return audioManager.transform.Find("VA").Find("Voice Line");
+            case AudioType.SFX:
+                return audioManager.transform.Find("SFX");
+            case AudioType.Conversation:
+                return audioManager.transform.Find("VA").Find("Conversation");
+            default:
+                Debug.LogWarning("Cannot find component under Audio Manager Prefab");
+                return null;
+        }
+    }
+
+    private void CreateVA(GameObject audioComp)
+    {
+        SoundBankVATrack soundBankVATrack = audioComp.AddComponent<SoundBankVATrack>();
+        soundBankVATrack.sounds = new List<UnityEngine.Object>();
+        soundBankVATrack.soundWeights = new List<float>();
+
+        foreach (AudioClip clip in clips)
+        {
+            AudioSource source = audioComp.AddComponent<AudioSource>();
+            source.clip = clip;
+            source.outputAudioMixerGroup = audioMixer.FindMatchingGroups(GetSelectedMixer())[0];
+            source.playOnAwake = false;
+
+            OneShotVATrack oneShotVATrack = audioComp.AddComponent<OneShotVATrack>();
+            oneShotVATrack.track = source;
+
+            soundBankVATrack.sounds.Add(oneShotVATrack);
+            soundBankVATrack.soundWeights.Add(1);
+        }
+
+    }
+
+    private void CreateSFX(GameObject audioComp)
+    {
+        switch (trackType)
+        {
+            case TrackType.OneShot: 
+                {
+                    AudioSource source = audioComp.AddComponent<AudioSource>();
+                    source.clip = clips[0];
+                    source.outputAudioMixerGroup = audioMixer.FindMatchingGroups(GetSelectedMixer())[0];
+                    source.playOnAwake = false;
+
+                    OneShotSFXTrack oneShotSFXTrack = audioComp.AddComponent<OneShotSFXTrack>();
+                    oneShotSFXTrack.track = source;
+                    break;
+                }
+            case TrackType.Looping:
+                {
+                    AudioSource source1;
+                    AudioSource source2;
+
+                    source1 = source2 = audioComp.AddComponent<AudioSource>();
+                    source1.clip = source2.clip = clips[0];
+                    source1.outputAudioMixerGroup = source2.outputAudioMixerGroup = audioMixer.FindMatchingGroups(GetSelectedMixer())[0];
+                    source1.playOnAwake = source2.playOnAwake = false;
+
+                    LoopingSFXTrack loopingSFXTrack = audioComp.AddComponent<LoopingSFXTrack>();
+                    loopingSFXTrack.tracks = new AudioSource[2];
+                    loopingSFXTrack.tracks[0] = source1;
+                    loopingSFXTrack.tracks[1] = source2;
+                    break;
+                }
+            case TrackType.SoundBank:
+                {
+                    SoundBankSFXTrack soundBankSFXTrack = audioComp.AddComponent<SoundBankSFXTrack>();
+                    soundBankSFXTrack.sounds = new List<UnityEngine.Object>();
+                    soundBankSFXTrack.soundWeights = new List<float>();
+
+                    foreach (AudioClip clip in clips)
+                    {
+                        AudioSource source = audioComp.AddComponent<AudioSource>();
+                        source.clip = clip;
+                        source.outputAudioMixerGroup = audioMixer.FindMatchingGroups(GetSelectedMixer())[0];
+                        source.playOnAwake = false;
+
+                        OneShotSFXTrack oneShotSFXTrack = audioComp.AddComponent<OneShotSFXTrack>();
+                        oneShotSFXTrack.track = source;
+
+                        soundBankSFXTrack.sounds.Add(oneShotSFXTrack);
+                        soundBankSFXTrack.soundWeights.Add(1);
+                    }
+                    break;
+                }
+        }
+    }
+
+    private void CreateConversation(GameObject audioComp)
+    {
+        ConversationAudioHolder conversationAudioHolder = audioComp.AddComponent<ConversationAudioHolder>();
+        conversationAudioHolder.tracks = new List<OneShotVATrack>();
+
+        foreach (AudioClip clip in clips)
+        {
+            AudioSource source = audioComp.AddComponent<AudioSource>();
+            OneShotVATrack oneShotVATrack = audioComp.AddComponent<OneShotVATrack>();
+
+            source.clip = clip;
+            source.outputAudioMixerGroup = audioMixer.FindMatchingGroups(GetSelectedMixer())[0];
+            source.playOnAwake = false;
+
+            oneShotVATrack.track = source;
+            conversationAudioHolder.tracks.Add(oneShotVATrack);
+        }
+    }
+
+    private string GetSelectedMixer()
+    {
+        switch (mixerType)
+        {
+            case Mixer.BIGSFX: return "BIGSFX";
+            case Mixer.AmbientSFX: return "AmbientSFX";
+            case Mixer.SFX: return "SFX";
+            case Mixer.PrioritySFX: return "PrioritySFX";
+            case Mixer.Music: return "Music";
+            case Mixer.DefaultSFX: return "DefaultSFX";
+            case Mixer.Master: return "Master";
+            case Mixer.VA: return "VA";
+            default:
+                Debug.LogWarning("Cannot find selected mixer");
+                return "";
+        }
     }
 }
 
