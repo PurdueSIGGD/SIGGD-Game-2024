50d354b982ab5d77eb835a82257a03a0b7484cca
Prisha Bangera <prishasbangera@gmail.com>
2025-06-15 00:54:33 -0500

    Everything except resistance seems to be workingg

--- Assets/Scripts/Ghosts/Specific Ghosts/Samurai/Upgrades/Ronins Resolve.cs
+++ Assets/Scripts/Ghosts/Specific Ghosts/Samurai/Upgrades/Ronins Resolve.cs
@@ -1,4 +1,5 @@
 #define DEBUG_LOG
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,6 +11,7 @@ using UnityEngine;
 public class RoninsResolve : Skill
 {
     private const string RESISTANCE_STAT = "Damage Resistance Percent Int"; // Resistance is percent as an int
+    private const string SPEED_STAT = "Max Running Speed";
 
     private static int pointIndex = 0;
 
@@ -60,14 +62,22 @@ public class RoninsResolve : Skill
     }
 
     /// <summary>
+    /// Calculate boost percent based on skill points
+    /// </summary>
+    /// <returns></returns>
+    private float CalculatePercent()
+    {
+        return pointIndex * 0.15f;
+    }
+
+    /// <summary>
     /// When Retribution is used, see whether we should add the speed/resistance boost
     /// </summary>
      
     private void HandleBoost(ActionContext context)
     {
-        Debug.Log(pointIndex + " " + context.extraContext.ToString());
         if (manager.selected && pointIndex > 0 &&
-            //context.actionID == ActionID.SAMURAI_SPECIAL &&
+            context.actionID == ActionID.SAMURAI_SPECIAL &&
             context.extraContext.Equals("Parry Success"))
         {
             if (boostTimer <= 0.0f) // Boost not in effect
@@ -93,9 +103,22 @@ public class RoninsResolve : Skill
     /// </summary>
     public void AddBoosts()
     {
+        float percent = CalculatePercent();
         boostTimer = boostDuration;
+
+        // Speed
+
+        addedSpeed = (int)(10 * percent * playerStats.ComputeValue(SPEED_STAT)); // FIXME
+        playerStats.ModifyStat(SPEED_STAT, addedSpeed);
+
+        // Resistance
+
+        playerStats.ModifyStat(RESISTANCE_STAT, (int)(percent * 100));
+
 #if DEBUG_LOG
-        Debug.Log("Ronin's Resolve: Boost added!");
+        Debug.Log("Ronin's Resolve: Boost added! " + percent * 100 + "%");
+        Debug.Log("Speed is now " + playerStats.ComputeValue(SPEED_STAT) +
+                  ", Resistance is now " + playerStats.ComputeValue(RESISTANCE_STAT) + "%");
 #endif
     }
 
@@ -105,8 +128,20 @@ public class RoninsResolve : Skill
     public void RemoveBoosts()
     {
         boostTimer = 0.0f;
+
+        // Speed
+
+        playerStats.ModifyStat(SPEED_STAT, -addedSpeed);
+        addedSpeed = 0;
+
+        // Resistance
+
+        playerStats.ModifyStat(RESISTANCE_STAT, -(int)(CalculatePercent() * 100));
+
 #if DEBUG_LOG
         Debug.Log("Ronin's Resolve: Boost removed!");
+        Debug.Log("Speed is now " + playerStats.ComputeValue(SPEED_STAT) +
+                  ", Resistance is now " + playerStats.ComputeValue(RESISTANCE_STAT) + "%");
 #endif
     }
 
