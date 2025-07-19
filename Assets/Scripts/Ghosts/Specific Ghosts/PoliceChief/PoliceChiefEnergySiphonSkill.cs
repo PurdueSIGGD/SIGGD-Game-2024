9ece96f92e7c586197490708728ca1a22d879400
Nathan Whitacre <whitacre@purdue.edu>
2025-07-19 13:03:25 -0400

    energy siphon change

--- Assets/Scripts/Ghosts/Specific Ghosts/PoliceChief/PoliceChiefEnergySiphonSkill.cs
+++ Assets/Scripts/Ghosts/Specific Ghosts/PoliceChief/PoliceChiefEnergySiphonSkill.cs
@@ -7,22 +7,25 @@ public class PoliceChiefEnergySiphonSkill : Skill
 {
     private StatManager stats;
     private PoliceChiefManager policeChiefManager;
-    private static float amountCooldownReduction = 0.0f;
-    [SerializeField] float[] values = { 0.0005f, 0.001f, 0.0015f, 0.002f };
+    [SerializeField] float[] values = {0f, 2f, 4f, 6f, 8f };
+    private int pointIndex = 0;
+
+    private float accumulatedCooldownReduction;
 
     private void Start()
     {
-        /*if(GetPoints() > 0)
-        {
-            amountCooldownReduction = pointCooldowns[GetPoints() - 1];
-        }
-        else
-        {
-            amountCooldownReduction = 0.0f;
-        }*/
-
         stats = GetComponent<StatManager>();
         policeChiefManager = GetComponent<PoliceChiefManager>();
+        accumulatedCooldownReduction = 0f;
+    }
+
+    private void Update()
+    {
+        if (accumulatedCooldownReduction > 0f && policeChiefManager.getSpecialCooldown() > 0f)
+        {
+            policeChiefManager.setSpecialCooldown(policeChiefManager.getSpecialCooldown() - accumulatedCooldownReduction);
+            accumulatedCooldownReduction = 0f;
+        }
     }
 
     private void OnEnable()
@@ -37,23 +40,33 @@ public class PoliceChiefEnergySiphonSkill : Skill
 
     public override void AddPointTrigger()
     {
-        amountCooldownReduction = values[GetPoints() - 1];
+        pointIndex = GetPoints();
     }
 
     public override void ClearPointsTrigger()
     {
-        amountCooldownReduction = 0.0f;
+        pointIndex = GetPoints();
     }
 
     public override void RemovePointTrigger()
     {
+        pointIndex = GetPoints();
     }
 
     void OnDmg(DamageContext context)
     {
-        if (context.attacker == PlayerID.instance.gameObject && context.actionID == ActionID.POLICE_CHIEF_BASIC)
+        if (context.attacker == PlayerID.instance.gameObject && context.actionID == ActionID.POLICE_CHIEF_SPECIAL)
         {
-            policeChiefManager.setSpecialCooldown(Mathf.Max(0, policeChiefManager.getSpecialCooldown() - (stats.ComputeValue("Special Cooldown") * amountCooldownReduction * context.damage)));
+            if (pointIndex <= 0) return;
+            float cooldownReduction = stats.ComputeValue("Special Cooldown") * (values[pointIndex] / 100f);
+            if (policeChiefManager.getSpecialCooldown() > 0)
+            {
+                policeChiefManager.setSpecialCooldown(policeChiefManager.getSpecialCooldown() - cooldownReduction);
+            }
+            else
+            {
+                accumulatedCooldownReduction += cooldownReduction;
+            }
         }
     }
 }
