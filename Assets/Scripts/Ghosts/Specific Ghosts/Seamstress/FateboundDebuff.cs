cc2b8f47737e8ac409955db3a8b738438da2a255
Prisha Bangera <prishasbangera@gmail.com>
2025-07-28 19:57:24 -0500

    Found reason for stack overflow?

--- Assets/Scripts/Ghosts/Specific Ghosts/Seamstress/FateboundDebuff.cs
+++ Assets/Scripts/Ghosts/Specific Ghosts/Seamstress/FateboundDebuff.cs
@@ -37,14 +37,11 @@ public class FateboundDebuff : MonoBehaviour
             // Handle Scrap Saver Skill
             manager.gameObject.GetComponent<ScrapSaver>().HandleEnemyDefeated();
 
-            manager.RemoveFromLink(gameObject.GetInstanceID());
-
             RemoveShareDamage();
 
-
-            // Handle Unraveled Fate Skill (must be after RemoveFromLink - otherwise stack overflow)
             manager.gameObject.GetComponent<UnraveledFate>().DamageFateboundEnemies(gameObject.GetInstanceID());
 
+            manager.RemoveFromLink(gameObject.GetInstanceID());
 
         }
     }
