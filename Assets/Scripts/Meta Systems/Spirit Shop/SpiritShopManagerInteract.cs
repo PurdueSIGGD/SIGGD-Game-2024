895d60839dcc815f409e915af03cca78cf8278ce
Prisha Bangera <prishasbangera@gmail.com>
2025-08-13 20:24:01 -0500

    renamed

--- Assets/Scripts/Meta Systems/Spirit Shop/SpiritShopManagerInteract.cs
+++ Assets/Scripts/Meta Systems/Spirit Shop/SpiritShopManagerInteract.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpiritManagerInteract : InRangeInteract
+public class SpiriShopManagerInteract : InRangeInteract
 {
     // ==============================
     //       Serialized Fields
