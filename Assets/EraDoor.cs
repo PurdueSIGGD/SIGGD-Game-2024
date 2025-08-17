4790690ea07f4de3b21e42b198d99d1455bd687e
xinmew <60266027+WangXin1507@users.noreply.github.com>
2025-08-17 10:30:04 -0400

    some more wip

--- Assets/EraDoor.cs
+++ Assets/EraDoor.cs
@@ -9,10 +9,12 @@ public class EraDoor : MonoBehaviour
     [SerializeField] List<SpecificLevelPool> specificLevels;
     [SerializeField] EnemySpawn[] enemies;
 
-    [SerializeField] Era era;
     //[SerializeField] int maxLevels = 20;
     //[SerializeField] int minPossibleStorybeatLoc = 3;
     //[SerializeField] int maxPossibleStorybeatLoc = 15;
+
+    [SerializeField] Era era;
+
     [SerializeField] int ghostOneFirstEncounterLoc = 3;
     [SerializeField] int ghostOneStoryBeatOne = 3;
     [SerializeField] int ghostOneStoryBeatTwo = 3;
@@ -20,12 +22,28 @@ public class EraDoor : MonoBehaviour
     [SerializeField] int ghostTwoStoryBeatOne = 6;
     [SerializeField] int ghostTwoStoryBeatTwo = 6;
 
+    private const string NORTH_NAME = "North-Police_Chief";
+    private const string EVA_NAME = "Eva-Idol";
+    private const string AKIHITO_NAME = "Akihito-Samurai";
+    private const string YUME_NAME = "Yume-Seamstress";
+    private const string SILAS_NAME = "Silas-PlagueDoc";
+    private const string AEGIS_NAME = "Aegis-King";
+
+
     // Rough progression of story progress:
     // 0: first encounter
-    // 1: Hub first entrance
+    // 1: Hub first entrance (autoprogress)
     // 2: Story beat 1
     // 3: Story beat 2
-    // 4: Story beat 3
+    // 4: Max Trust
+    // 5: Story beat 3
+
+    private enum storyProgression
+    {
+        First_Encounter,
+        Hub_First_Entrance,
+        Story_Beat_1,
+    }
 
     void DoorOpened()
     {
@@ -33,19 +51,22 @@ public class EraDoor : MonoBehaviour
         if (era == Era.Cyberpunk)
         {
             // North
-            if (SaveManager.data.north.storyProgress == 0) // North First Encounter
+            if (SaveManager.data.north.storyProgress == (int)storyProgression.First_Encounter) // North First Encounter
             {
                 SpecificLevelPool pool = new(new Level[] { new("North First Encounter", 1) }, ghostOneFirstEncounterLoc);
                 specificLevels.Add(pool);
             }
             else if (PartyManager.instance.IsGhostInParty("North-Police_Chief"))
             {
-                if (SaveManager.data.north.storyProgress == 1 || SaveManager.data.north.storyProgress == 2) // North story beat 1
+                if ((SaveManager.data.north.storyProgress == (int)storyProgression.Hub_First_Entrance 
+                     || SaveManager.data.north.storyProgress == (int)storyProgression.Story_Beat_1)
+                     && SaveManager.data.ghostLevel[NORTH_NAME] >= 4) // North story beat 1
                 {
                     SpecificLevelPool pool = new(new Level[] { new("North Story Beat One", 1) }, ghostOneStoryBeatOne);
                     specificLevels.Add(pool);
                 }
-                else if (SaveManager.data.north.storyProgress == 3) // North story beat 2
+                else if (SaveManager.data.north.storyProgress == 3
+                         && SaveManager.data.ghostLevel[NORTH_NAME] >= 7) // North story beat 2
                 {
                     SpecificLevelPool pool = new(new Level[] { new("North Story Beat Two", 1) }, ghostOneStoryBeatTwo);
                     specificLevels.Add(pool);
@@ -60,7 +81,8 @@ public class EraDoor : MonoBehaviour
             }
             else if (PartyManager.instance.IsGhostInParty("Eva-Idol"))
             {
-                if (SaveManager.data.eva.storyProgress == 1 || SaveManager.data.eva.storyProgress == 2) // Eva story beat 1
+                if ((SaveManager.data.eva.storyProgress == 1 || SaveManager.data.eva.storyProgress == 2)
+                     && SaveManager.data.ghostLevel[EVA_NAME] >= 4) // Eva story beat 1
                 {
                     SpecificLevelPool pool = new(new Level[] { new("Eva Story Beat One", 1) }, ghostTwoStoryBeatOne);
                     specificLevels.Add(pool);
@@ -163,6 +185,42 @@ public class EraDoor : MonoBehaviour
         LevelSwitching.specificLevels = specificLevels.ToArray();
         EnemySpawning.enemies = enemies;
     }
+
+    private void InjectStoryBeat()
+    {
+        GhostData data1;
+        GhostData data2;
+
+        switch(era)
+        {
+            case Era.Cyberpunk:
+                data1 = SaveManager.data.north;
+                data2 = SaveManager.data.eva;
+                break;
+            case Era.Feudal:
+                data1 = SaveManager.data.akihito;
+                data2 = SaveManager.data.yume;
+                break;
+            case Era.Medieval:
+                data1 = SaveManager.data.silas;
+                data2 = SaveManager.data.aegis;
+                break;
+            default:
+                return; // do not inject if Misc
+        }
+    }
+
+    private void ChooseStoryBeats(GhostData data1, GhostData data2, string name1, string name2)
+    {
+        string trunc_name1 = name1.Split('-')[0];
+        string trunc_name2 = name2.Split('-')[0];
+        
+        if (data1.storyProgress == (int)storyProgression.First_Encounter) // first encounter
+        {
+            specificLevels.Add(new(new Level[] { new(trunc_name1 + " First Encounter", 1) }, ghostOneFirstEncounterLoc));
+        }
+    }
+
 
     // can be used to generate a random index for story beat to occur, currently not in use
     //private int GenerateStorybeatLocation()
