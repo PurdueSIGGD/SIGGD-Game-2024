fa5c30288ea8ae4176492b569c03dc7a7a0298de
Daniel Ng <ng17daniel@gmail.com>
2025-08-18 18:54:21 -0400

    noboru fight done

--- Assets/Scripts/Enemies/BossScripts/Noboru/NoboruController.cs
+++ Assets/Scripts/Enemies/BossScripts/Noboru/NoboruController.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class NoboruController : BossController
 {
+    EnemyStateManager enemyStateManager;
+    Animator anim;
+    [SerializeField] float deathTimer;
+
+    public void Start()
+    {
+        base.Start();
+        enemyStateManager = GetComponent<EnemyStateManager>();
+        anim = GetComponent<Animator>();
+    }
     public void SpawnYokai(GameObject yokaiPrefab, GameObject enemy)
     {
         SpawnEnemyAtRandomPoint(enemy, yokaiPrefab);
@@ -13,5 +23,15 @@ public class NoboruController : BossController
     {
         base.DefeatSequence();
         EnableInvincibility();
+        enemyStateManager.enabled = false;
+        anim.SetTrigger("dead");
+        StartCoroutine(DefeatCoroutine());
+    }
+    IEnumerator DefeatCoroutine()
+    {
+        print("Maybe... maybe I shouldn't have been the bad guy... maybe I shouldn't have killed *ALL* of them...");
+        yield return new WaitForSeconds(deathTimer);
+        Destroy(gameObject);
+        EndBossRoom();
     }
 }
